38a7e71975903f9419b777d69329e7a9145a2966
Jeremy Azzari <jeremy@stacksocial.com>
2016-10-16 22:26:14 -0700

    added fib solution

--- src/climbing_stairs.js
+++ src/climbing_stairs.js
@@ -4,36 +4,47 @@
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you
 // climb to the top?
 
-function fact(n, total=1) {
-  if( n === 0 )
-    return total
+// Organically arrived solution:
+//
+// function fact(n, total=1) {
+//   if( n === 0 )
+//     return total
+//
+//   return fact(n-1, total*n)
+// }
+//
+// function permute(args) {
+//   let d = args.reduce((acc, v) => {
+//     return acc *= fact(v)
+//   }, 1)
+//   let n = args.reduce((acc, v) => {
+//     return acc+=v
+//   }, 0)
+//   return fact(n) / d
+// }
+//
+// function climbStairs(n) {
+//   if( n<=3 ) return n
+//   let ways = 0
+//   let ones = n
+//   let twos = 0
+//
+//   while(ones >= 0) {
+//     ways += permute([ones, twos])
+//     ones -= 2
+//     twos ++
+//   }
+//
+//   return ways
+// }
 
-  return fact(n-1, total*n)
-}
-
-function permute(args) {
-  let d = args.reduce((acc, v) => {
-    return acc *= fact(v)
-  }, 1)
-  let n = args.reduce((acc, v) => {
-    return acc+=v
-  }, 0)
-  return fact(n) / d
-}
-
+// Just happens to be the fibonacci sequence :P
 function climbStairs(n) {
-  if( n<=3 ) return n
-  let ways = 0
-  let ones = n
-  let twos = 0
-
-  while(ones >= 0) {
-    ways += permute([ones, twos])
-    ones -= 2
-    twos ++
+  var steps = [0,1,2]
+  for(var i=3; i<=n; i++) {
+    steps[i] = steps[i-1] + steps[i-2]
   }
-
-  return ways
+  return steps[n]
 }
 
 module.exports = climbStairs
