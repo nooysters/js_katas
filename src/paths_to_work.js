7f36f3898852ec28ed6f51cc71234d1c2590e56d
Jeremy Azzari <jeremy@stacksocial.com>
2016-10-17 18:16:15 -0700

    added numbers  to words

--- src/paths_to_work.js
+++ src/paths_to_work.js
@@ -5,24 +5,20 @@
 //
 // Try utilizing some kind of lookup table or memoization to optimize the time
 // complexity.
-
 function pathsToWork(x, y) {
   if(!x || !y) return 1
-  var map = Array(x+1).fill().map( ()=>{ return Array(y+1).fill(0) } )
-  var routes = 0
+
+  let map = Array(x+1).fill().map( ()=>{ return Array(y+1).fill(0) } )
+  let routes = 0
   map[x][y] = 1 // sets work
 
-  let searchRoute = (s, e) => {
-    if( !map[s] ) return
-    if( map[s][e] === 1 || s === x || e === y ) {
-      return routes ++
-    }
-    if( map[s][e] === 0 ) {
-      searchRoute(s+1, e)
-      searchRoute(s, e+1)
-    }
-  }
   searchRoute(0, 0)
+
+  function searchRoute(s, e) {
+    if( !map[s] ) return
+    if( map[s][e] === 1 || s === x || e === y ) return routes ++
+    if( map[s][e] === 0 ) searchRoute(s+1, e), searchRoute(s, e+1)
+  }
 
   return routes
 }
